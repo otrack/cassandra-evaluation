2d38b590d0b7a1a0a4f9a27aa02ed4fc5889ae6c
otrack <0track@gmail.com>
2025-06-17 11:19:41 +0200

    update

--- fmt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# install https://github.com/mvdan/sh
-
-DIR=$(dirname "${BASH_SOURCE[0]}")
-
-for f in $(ls -d ${DIR}/*.sh ${DIR}/../paper/experiments/*.sh ../paper/experiments/*/*.sh ${DIR}/latency/*.sh); do
-    shfmt -i 4 -w ${f}
-done
--- list_status.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$(dirname "${BASH_SOURCE[0]}")
-${DIR}/list.sh | grep -Eio "(running|error)" | sort | uniq -c
