feed1e916b4c223e0f49282b405062df9aef1109
otrack <0track@gmail.com>
2026-03-02 00:03:45 +0100

    update

--- Docker/latency_throughput.sh
+++ Docker/latency_throughput.sh
@@ -49,7 +49,7 @@ do
         do_create_and_load=0
 
         # Check if average latency exceeded 1s (1,000,000 us); if so, stop increasing threads
-        max_avg_latency=$(awk -F',' '/AverageLatency\(us\)/{lat=$3; gsub(/[[:space:]]/,"",lat); if(lat+0>max) max=lat+0} END{print max+0}' "${output_file%.dat}_Hanoi.dat"") 
+        max_avg_latency=$(awk -F',' '/AverageLatency\(us\)/{lat=$3; gsub(/[[:space:]]/,"",lat); if(lat+0>max) max=lat+0} END{print max+0}' "${output_file%.dat}_Hanoi.dat") 
         if [ "${max_avg_latency}" -gt 1000000 ]; then
             log "Average latency ${max_avg_latency}us exceeds 1s for protocol ${p}, stopping thread increase"
             break
