98bf0b2844b240935ccfd2653c7697b890726a28
otrack <0track@gmail.com>
2026-03-03 18:48:45 +0100

    update

--- Docker/latency_throughput.sh
+++ Docker/latency_throughput.sh
@@ -49,10 +49,10 @@ do
         run_benchmark ${p} ${threads} ${nodes} ${replication_factor} ${workload_type} ${workload} ${records} $((threads * ops_per_thread)) ${output_file} ${do_create_and_load} ${do_clean_up} -p conflict.theta=${theta} -p updateproportion=1.0 -p readproportion=0.0
         do_create_and_load=0
 
-        # Check if average latency exceeded 1s (1,000,000 us); if so, stop increasing threads
+        # Check if average latency exceeded 1s (500,000 us); if so, stop increasing threads
 	city=$(cat latencies.csv | head -n 2 | tail -n 1 | awk -F, '{print $3}')
         max_avg_latency=$(cat "${output_file%.dat}_${city}.dat" | grep -v CLEANUP | awk -F',' '/AverageLatency\(us\)/{lat=$3; gsub(/[[:space:]]/,"",lat); if(lat+0>max) max=lat+0} END{print max+0}')
-        if [ "${max_avg_latency}" -gt 1000000 ]; then
+        if [ "${max_avg_latency}" -gt 500000 ]; then
             log "Average latency ${max_avg_latency}us exceeds 1s for protocol ${p}, stopping thread increase"
             break
         fi
