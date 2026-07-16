648f2dfd45c144d25467d3ef5ea9ca1f87d8323d
otrack <0track@gmail.com>
2026-07-16 21:20:36 +0200

    update

--- Docker/utils.sh
+++ Docker/utils.sh
@@ -317,10 +317,6 @@ pull_images() {
         [[ -z "$key" || "$key" =~ ^[[:space:]]*# ]] && continue
         [[ "$key" =~ _image$ ]] || continue
         value=$(echo "$value" | xargs)
-        if [[ "$value" == 0track/tiga-suite* || "$value" == 0track/ycsb* ]]; then
-            log "Skipping pull for local image: ${value}"
-            continue
-        fi
         log "Pulling image: ${value}"
         docker pull "${value}"
         if [ $? -ne 0 ]; then
